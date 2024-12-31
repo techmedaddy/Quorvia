a873cd7014e4e48d4f7174939435c94d72b567ac
rajababu <123633963+umarei@users.noreply.github.com>
2024-12-31 17:28:01 +0530

    Add conditional database creation in init.sql to prevent errors if database already exists

--- init.sql
+++ init.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE quorvia_user;
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'quorvia_user') THEN
+      CREATE DATABASE quorvia_user;
+   END IF;
+END
+$$;
